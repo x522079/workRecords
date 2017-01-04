e4554c2a72bdba7fcacef763d74db152c20e45a1
michael <522079893@qq.com>
2017-01-04 21:30:16 +0800

    db

--- advanced/common/config/main-local.php
+++ advanced/common/config/main-local.php
@@ -3,9 +3,9 @@ return [
     'components' => [
         'db' => [
             'class' => 'yii\db\Connection',
-            'dsn' => 'mysql:host=localhost;dbname=yii2advanced',
+            'dsn' => 'mysql:host=localhost;dbname=db_records',
             'username' => 'root',
-            'password' => '',
+            'password' => 'michael',
             'charset' => 'utf8',
         ],
         'mailer' => [
